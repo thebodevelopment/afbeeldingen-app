05fb5e7f031f8f152e657e82dfebe9c64c75877f
thebodevelopment <85729963+thebodevelopment@users.noreply.github.com>
2023-03-24 13:13:08 +0100

    Added user parameters

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ import utils
 
 st.title('THEBO Afbeeldingen')
 
+# User parameters
+WIDTH = st.number_input('Breedte afbeelding:', min_value=100, max_value=2000, value=800, step=50)
+HEIGHT = st.number_input('Hoogte afbeelding:', min_value=100, max_value=2000, value=800, step=50)
+QUALITY = st.number_input('Afbeeldingskwaliteit:', min_value=1, max_value=95, value=50, step=5)
+
 uploaded_files = st.file_uploader('Selecteer afbeeldingen...', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
 processed_images = []
 
@@ -16,7 +21,7 @@ if uploaded_files is not None:
     for uploaded_file in uploaded_files:
         image = utils.open_image(uploaded_file)
         image = utils.trim(image)
-        image = utils.resize(image, 800, 800)
+        image = utils.resize(image, WIDTH, HEIGHT)
         image.filename = uploaded_file.name
         processed_images.append(image)
 
@@ -26,7 +31,7 @@ if st.button('Download afbeeldingen als zip'):
         for processed_image in processed_images:
             image_name = processed_image.filename
             image_bytes = io.BytesIO()
-            processed_image.save(image_bytes, format='PNG')
+            processed_image.save(image_bytes, format='PNG', quality=QUALITY, optimize=True)
             z.writestr(image_name, image_bytes.getvalue())
     zip_file.seek(0)
     st.download_button('Download', data=zip_file.getvalue(), file_name='gecorrigeerde_afbeeldingen.zip', mime='application/zip')
